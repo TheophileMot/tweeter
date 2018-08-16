c781f53df3163677a148bbe07c16bcecb0118c91
Timothy Mott <tjbmott+github@gmail.com>
2018-08-16 18:54:00 -0400

    got rid of fake database; connected to Mongo

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -8,38 +8,43 @@ function createErrorMessage(msg) {
   return $('<p>').addClass('error-msg').text(msg);
 }
 
-function age(tweet) {
-  return new Date() - tweet.createdAt;
-}
-
-function howLongAgo(tweet) {
-  return (age(tweet) / (365 * 24 * 60 * 60 * 1000)).toPrecision(3) + ' years ago';
+function howLongAgo(time) {
+  let age = new Date() - time;
+  return (age / (365 * 24 * 60 * 60 * 1000)).toPrecision(3) + ' years ago';
 }
 
 function createTweetElement(tweet) {
-  let $tweet = $('<article>');
+  // check whether database object has the right fields
+  if (!tweet.user || !tweet.content || !tweet.createdAt) {
+    for (let prop of ['user', 'content', 'createdAt']) {
+      if (!tweet[prop]) { console.log(`Database error! Property ${prop} missing in db entry ${tweet._id}`); }
+    }
+    return $('<article>check your database for errors</article>');
+  } else {
+    let $tweet = $('<article>');
 
-  let $header = $('<header>');
-  let $header_img_avatar = $('<img>').addClass('avatar').attr('src', tweet.user.avatars.small);
-  let $header_h2_username = $('<h2>').addClass('username').text(tweet.user.name);
-  let $header_span_handle = $('<span>').addClass('handle').text(tweet.user.handle);
-  $header.append($header_img_avatar, $header_h2_username, $header_span_handle);
-  
-  let $message = $('<div class="message">');
-  $message.text(tweet.content.text);
+    let $header = $('<header>');
+    let $header_img_avatar = $('<img>').addClass('avatar').attr('src', tweet.user.avatars.small);
+    let $header_h2_username = $('<h2>').addClass('username').text(tweet.user.name);
+    let $header_span_handle = $('<span>').addClass('handle').text(tweet.user.handle);
+    $header.append($header_img_avatar, $header_h2_username, $header_span_handle);
+    
+    let $message = $('<div class="message">');
+    $message.text(tweet.content.text);
 
-  let $footer = $('<footer>');
-  let $footer_span_date = $('<span>').addClass('date').text(howLongAgo(tweet));
-  let $footer_span_icons = $('<span>').addClass('icons');
-  $footer_span_icons.append('<i class="fas fa-flag"></i> <i class="fas fa-recycle"></i> <i class="fas fa-heart"></i>');
-  $footer.append($footer_span_date, $footer_span_icons);
+    let $footer = $('<footer>');
+    let $footer_span_date = $('<span>').addClass('date').text(howLongAgo(tweet.createdAt));
+    let $footer_span_icons = $('<span>').addClass('icons');
+    $footer_span_icons.append('<i class="fas fa-flag"></i> <i class="fas fa-recycle"></i> <i class="fas fa-heart"></i>');
+    $footer.append($footer_span_date, $footer_span_icons);
 
-  let $img = $('<img>');
-  $img.addClass('avatar');
+    let $img = $('<img>');
+    $img.addClass('avatar');
 
-  $tweet.append($header, $message, $footer);
+    $tweet.append($header, $message, $footer);
 
-  return $tweet;
+    return $tweet;
+  }
 }
 
 // render a tweet or an array of tweets
@@ -47,7 +52,13 @@ function renderTweets(tweetData) {
   if (Array.isArray(tweetData)) {
     tweetData.forEach( tweet => renderTweets(tweet) );
   } else {
-    $('section.tweet-list').prepend(createTweetElement(tweetData) );
+    let $tweetElement = createTweetElement(tweetData);
+    if ($tweetElement) {
+      $('section.tweet-list').prepend($tweetElement);
+    } else {
+      $('section.tweet-list').prepend($tweetElement);
+      //
+    }
   }
 }
 
--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -3,25 +3,26 @@
 // Simulates the kind of delay we see with network or filesystem operations
 const simulateDelay = require('./util/simulate-delay');
 
+
 // Defines helper functions for saving and getting tweets, using the database `db`
 module.exports = function makeDataHelpers(db) {
   return {
 
     // Saves a tweet to `db`
     saveTweet: function(newTweet, callback) {
-      simulateDelay(() => {
-        db.tweets.push(newTweet);
-        callback(null, true);
-      });
+      db.collection('tweets').insert(newTweet, callback);
     },
 
     // Get all tweets in `db`, sorted by newest first
     getTweets: function(callback) {
-      simulateDelay(() => {
-        const sortNewestFirst = (a, b) => a.createdAt - b.createdAt;
-        callback(null, db.tweets.sort(sortNewestFirst));
+      const sortNewestFirst = (a, b) => a.createdAt - b.createdAt;
+      db.collection('tweets').find().toArray((err, data) => {
+        if (err) {
+          throw err;
+        } else {
+          callback(null, data.sort(sortNewestFirst));
+        }
       });
     }
-
   };
 };
